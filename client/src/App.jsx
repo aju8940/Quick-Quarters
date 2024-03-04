44c61fffdec8ad9b9494be4194df7eefdc8344de
Ajmal Latheef <ajmallatheef8940@gmail.com>
2024-03-04 13:50:20 +0530

    SERVER Created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import Home from "./pages/Home";
 import { About } from "./pages/About";
-import SingIn from "./pages/SingIn";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SingIn />} />
+        <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
